83046251460f82c58eb3a08bcff27025b30383c7
KK <KK_Chen@syscom.com.tw>
2017-09-20 17:41:37 +0800

    recorrect httpget

--- MyMVC/Controllers/HomeController.cs
+++ MyMVC/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MyMVC.Models;
 using MyMVC.Services;
 
 namespace MyMVC.Controllers
@@ -32,6 +33,28 @@ namespace MyMVC.Controllers
         {
             var members = MemberSvc.GetMembers();
             return View(members);
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            MemberViewModel obj = new MemberViewModel();
+            return View(obj);
+        }
+
+        [HttpPost]
+        public ActionResult Create(MemberViewModel obj)
+        {
+            //if (obj.ID == null)
+            //{
+            //    obj = new MemberViewModel();
+            //    return View(obj);
+            //}
+            //else
+            {
+                MemberSvc.Add(obj);
+                return RedirectToAction("Member");
+            }
         }
     }
 }
